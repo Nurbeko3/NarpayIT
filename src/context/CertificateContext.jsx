60eb36454f49511b05be58a355e5e35dd3addfae
Nurbeko3 <nurkn177@gmail.com>
2026-02-23 22:55:36 +0500

    chore: updated backend api url

--- src/context/CertificateContext.jsx
+++ src/context/CertificateContext.jsx
@@ -12,7 +12,7 @@ export const CertificateProvider = ({ children }) => {
   useEffect(() => {
     const fetchCertificates = async () => {
       try {
-        const response = await fetch("https://api-narpay-backend.onrender.com/api/certificates");
+        const response = await fetch("https://api-narpay.onrender.com/api/certificates");
         if (!response.ok) {
           throw new Error("Failed to fetch certificates");
         }
